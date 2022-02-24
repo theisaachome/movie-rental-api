c502f69188393aa0a19f7f867ce9fa04dc759771
theisaachome <65864160+theisaachome@users.noreply.github.com>
2022-02-25 01:46:04 +0630

    Model Validator setup

--- src/model/Movie.js
+++ src/model/Movie.js
@@ -30,8 +30,15 @@ const MovieSchema = new mongoose.Schema(
   }
 );
 
+const Movie = mongoose.model("Movie",MovieSchema);
 function validateMovie(movie) {
- 
+  const schema = Joi.object({
+    title: Joi.string().min(5).max(50).required(),
+    genreId: Joi.objectId().required(),
+    numberInStock: Joi.number().min(0).required(),
+    dailyRentalRate: Joi.number().min(0).required()
+  });
+  return schema.validate(movie);
 }
 
 exports.Movie = Movie; 
