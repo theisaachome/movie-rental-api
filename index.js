1e6850ff713ec2881bbfefbe89a8d4f3ffe63670
theisaachome <65864160+theisaachome@users.noreply.github.com>
2022-02-25 19:55:42 +0630

    mount api route

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ const app = express();
 const genres = require('./src/routes/genres');
 const customers = require('./src/routes/customers');
 const movies = require('./src/routes/movies');
+const rentals = require('./src/routes/rentals');
 
 //Set up default mongoose connection
 const mongoDB = 'mongodb://127.0.0.1/movierentalDB';
@@ -20,6 +21,7 @@ app.use(morgan("dev"));
 app.use('/api/v1/genres',genres);
 app.use('/api/v1/customers',customers);
 app.use('/api/v1/movies',movies);
+app.use('/api/v1/rentals',rentals);
 
 app.listen(5000,()=>{
     console.log("App is running on ");
