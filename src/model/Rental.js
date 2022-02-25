cd46d2296587f96cba47cd3cd932bfc25fae9083
theisaachome <65864160+theisaachome@users.noreply.github.com>
2022-02-25 19:25:49 +0630

    rental model and validator

--- src/model/Rental.js
+++ src/model/Rental.js
@@ -56,12 +56,11 @@ const Rental = mongoose.model('Rental', new mongoose.Schema({
 }));
 
 function validateRental(rental) {
-  const schema = {
+  const schema = Joi.object({
     customerId: Joi.objectId().required(),
     movieId: Joi.objectId().required()
-  };
-
-  return Joi.validate(rental, schema);
+  });
+  return schema.validate(rental);
 }
 
 exports.Rental = Rental; 
